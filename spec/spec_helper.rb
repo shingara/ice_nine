c32712987483495cb1c1407745f9d1cd84a8ff76
Dan Kubb <dan.kubb@gmail.com>
2012-03-24 23:34:50 -0700

    Add rspec/autorun to spec helper to fix broken rcov output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 require 'rspec'
+require 'rspec/autorun'
 
 # require spec support files and shared behavior
 Dir[File.expand_path('../shared/**/*.rb', __FILE__)].each do |file|
